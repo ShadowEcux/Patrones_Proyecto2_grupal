234e9aa269b74131ac987f14bdfa6642e57cb051
Collantes Morales Joseph <josephc_fc@hotmail.com>
2022-11-05 07:44:07 -0500

    subir bd
    base de datos nueva

--- db/dbsistema.sql
+++ db/dbsistema.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-06-2017 a las 06:01:45
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.6.15
+-- Tiempo de generación: 05-11-2022 a las 13:43:03
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -34,8 +35,27 @@ CREATE TABLE `articulo` (
   `stock` int(11) NOT NULL,
   `descripcion` varchar(256) DEFAULT NULL,
   `imagen` varchar(50) DEFAULT NULL,
-  `condicion` tinyint(1) NOT NULL DEFAULT '1'
+  `condicion` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `articulo`
+--
+
+INSERT INTO `articulo` (`idarticulo`, `idcategoria`, `codigo`, `nombre`, `stock`, `descripcion`, `imagen`, `condicion`) VALUES
+(1, 1, 'CEL-00001-Z', 'REDMI 10 2020 4GB+128GB WHITE', 50, 'Solo disponible en dolor Negro y Azul', '1667651940.jpg', 1),
+(2, 1, 'CEL-00002-Z', 'iPhone 11 128GB', 30, 'Disponible solo en colores verde y rosado', '1667651957.jpg', 1),
+(3, 1, 'CEL-00003-Z', 'Galaxy A04 Negro', 35, 'Precio con 10%dsct', '1667651968.jpg', 1),
+(4, 1, 'CEL-00004-Z', 'iPhone 13 Pro 128GB', 15, 'Precio con 15%dsct', '1667651981.jpg', 1),
+(5, 1, 'CEL-00005-Z', 'Redmi 10C 3G+64GB', 42, 'Top Ventas', '1667651991.jpg', 1),
+(6, 1, 'CEL-00006-Z', 'Memoria Microsd C/Adap Sd 32gb', 64, 'Marca. Maxell', '1667652001.jpg', 1),
+(7, 4, 'AUD-00001-Z', 'Audífonos Skullcandy Dime True wireless', 24, 'Solo disponible en dolor Negro y Blanco', '1667651875.jpg', 1),
+(8, 4, 'AUD-00002-Z', 'JBL SPEAKER BOOMBOX2 BLUETOOTH BLK', 22, '', '1667651890.jpg', 1),
+(9, 4, 'AUD-00003-Z', 'Audífonos inalámbricos Kids con luces led PowerLocus Lila', 11, 'Solo color Lila', '1667651908.jpg', 1),
+(10, 4, 'AUD-00004-Z', 'LG Minicomponente Bluetooth DJ XBOOM OK99', 7, 'No incluye adicionales', '1667651930.jpg', 1),
+(11, 2, 'COM-00001-Z', 'Impresora Multifuncional WiFi Ecotank L5290', 56, 'Alto: 23,7 cm | Ancho: 34,7', '1667652011.jpg', 1),
+(12, 2, 'COM-00002-Z', 'Mouse Gamer 7 Botones Micronics Winner Mic-m802 Rgb', 57, 'Incluye luz multi led', '1667652021.jpg', 1),
+(13, 3, 'VJU-00001-Z', 'PS5 HW STANDARD - LATAM', 34, 'Entradas HDMI: Compatibilidad con televisores 4K de 120Hz, televisores 8K, VRR (especificado por HDMI ver2.1)', '1667652031.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -47,8 +67,18 @@ CREATE TABLE `categoria` (
   `idcategoria` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `descripcion` varchar(256) DEFAULT NULL,
-  `condicion` tinyint(1) NOT NULL DEFAULT '1'
+  `condicion` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`idcategoria`, `nombre`, `descripcion`, `condicion`) VALUES
+(1, 'Celulares', 'Categoría de accesorios y cases, celulares, cargadores y powerbanks.', 1),
+(2, 'Computadoras', 'Categooría de accesorios, almacenamiento, laptops, monitores, routers y repetidores WiFi, Impresoras y Tintas.', 1),
+(3, 'Videojuegos', 'Categoría de accesorios, consolas, juegos, Nintendo, PlayStation, Mandos, Xbox.', 1),
+(4, 'Audio', 'Categoría de accesorios, audífonos, audio profesional y DJ, autoradios, parlantes, equipos de sonido.', 1);
 
 -- --------------------------------------------------------
 
@@ -162,6 +192,16 @@ CREATE TABLE `persona` (
   `email` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`idpersona`, `tipo_persona`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`) VALUES
+(1, 'Proveedor', 'Proveedor CEL - Nicolas Uribe', 'DNI', '60606060', 'Mz A3 Lote 2 Comité 20', '666666666', 'ur.nico@gmail.com'),
+(2, 'Proveedor', 'Proveedor COM - Ana María Mendoza', 'DNI', '70707070', 'Mz C5 Lote 15 Comité 2', '777777777', 'me.anamaria@gmail.com'),
+(3, 'Proveedor', 'Proveedor  AUD - Milena Saenz', 'DNI', '80808080', 'Mz B5 Lote 12 Comité 5', '888888888', 'sa.milena@gmail.com'),
+(4, 'Proveedor', 'Proveedor VJU - Juliana Cornejo', 'DNI', '90909090', 'Jr. Miro Quesada 127', '999999999', 'co.juliana@gmail.com');
+
 -- --------------------------------------------------------
 
 --
@@ -180,7 +220,7 @@ CREATE TABLE `usuario` (
   `login` varchar(20) NOT NULL,
   `clave` varchar(64) NOT NULL,
   `imagen` varchar(50) NOT NULL,
-  `condicion` tinyint(1) NOT NULL DEFAULT '1'
+  `condicion` tinyint(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -188,7 +228,11 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`idusuario`, `nombre`, `tipo_documento`, `num_documento`, `direccion`, `telefono`, `email`, `cargo`, `login`, `clave`, `imagen`, `condicion`) VALUES
-(1, 'Juan Carlos Arcila', 'DNI', '47715777', 'Jose Gálvez 1368 - Chongoyape', '931742904', '', '', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '1487132068.jpg', 1);
+(1, 'Giannina Bernal', 'DNI', '10101010', 'Dirección 1', '111111111', '', '', 'begiannina', '8442af777e10f2da9f3b73e1daa0854b426cbdd833086c980698704c426ad5af', '1667651548.png', 1),
+(2, 'Joseph Collantes', 'DNI', '20202020', 'Dirección2', '222222222', '', '', 'cojoseph', '4cab97e818b20e04b3e8530034ab018229ef6e3c090aa11d38b752e1a7e69b7c', '1667651600.png', 1),
+(3, 'Joseph Cambillo', 'DNI', '30303030', 'Dirección 3', '333333333', '', '', 'cajoseph', '4cab97e818b20e04b3e8530034ab018229ef6e3c090aa11d38b752e1a7e69b7c', '1667651611.png', 1),
+(4, 'Oriana Ramos', 'DNI', '40404040', 'Dirección 4', '444444444', '', '', 'raoriana', '4cab97e818b20e04b3e8530034ab018229ef6e3c090aa11d38b752e1a7e69b7c', '1667651628.png', 1),
+(5, 'Carlos Carbajal', 'DNI', '50505050', 'Dirección 5', '555555555', '', '', 'cacarlos', '8442af777e10f2da9f3b73e1daa0854b426cbdd833086c980698704c426ad5af', '1667651636.png', 1);
 
 -- --------------------------------------------------------
 
@@ -207,13 +251,41 @@ CREATE TABLE `usuario_permiso` (
 --
 
 INSERT INTO `usuario_permiso` (`idusuario_permiso`, `idusuario`, `idpermiso`) VALUES
-(96, 1, 1),
-(97, 1, 2),
-(98, 1, 3),
-(99, 1, 4),
-(100, 1, 5),
-(101, 1, 6),
-(102, 1, 7);
+(152, 1, 1),
+(153, 1, 2),
+(154, 1, 3),
+(155, 1, 4),
+(156, 1, 5),
+(157, 1, 6),
+(158, 1, 7),
+(159, 2, 1),
+(160, 2, 2),
+(161, 2, 3),
+(162, 2, 4),
+(163, 2, 5),
+(164, 2, 6),
+(165, 2, 7),
+(166, 3, 1),
+(167, 3, 2),
+(168, 3, 3),
+(169, 3, 4),
+(170, 3, 5),
+(171, 3, 6),
+(172, 3, 7),
+(180, 4, 1),
+(181, 4, 2),
+(182, 4, 3),
+(183, 4, 4),
+(184, 4, 5),
+(185, 4, 6),
+(186, 4, 7),
+(187, 5, 1),
+(188, 5, 2),
+(189, 5, 3),
+(190, 5, 4),
+(191, 5, 5),
+(192, 5, 6),
+(193, 5, 7);
 
 -- --------------------------------------------------------
 
@@ -320,52 +392,62 @@ ALTER TABLE `venta`
 -- AUTO_INCREMENT de la tabla `articulo`
 --
 ALTER TABLE `articulo`
-  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idarticulo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
 --
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT de la tabla `detalle_ingreso`
 --
 ALTER TABLE `detalle_ingreso`
   MODIFY `iddetalle_ingreso` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `detalle_venta`
 --
 ALTER TABLE `detalle_venta`
   MODIFY `iddetalle_venta` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `ingreso`
 --
 ALTER TABLE `ingreso`
   MODIFY `idingreso` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- AUTO_INCREMENT de la tabla `permiso`
 --
 ALTER TABLE `permiso`
   MODIFY `idpermiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
 --
 -- AUTO_INCREMENT de la tabla `persona`
 --
 ALTER TABLE `persona`
-  MODIFY `idpersona` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idpersona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idusuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
 --
 -- AUTO_INCREMENT de la tabla `usuario_permiso`
 --
 ALTER TABLE `usuario_permiso`
-  MODIFY `idusuario_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=103;
+  MODIFY `idusuario_permiso` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=194;
+
 --
 -- AUTO_INCREMENT de la tabla `venta`
 --
 ALTER TABLE `venta`
   MODIFY `idventa` int(11) NOT NULL AUTO_INCREMENT;
+
 --
 -- Restricciones para tablas volcadas
 --
@@ -410,6 +492,7 @@ ALTER TABLE `usuario_permiso`
 ALTER TABLE `venta`
   ADD CONSTRAINT `fk_venta_persona` FOREIGN KEY (`idcliente`) REFERENCES `persona` (`idpersona`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   ADD CONSTRAINT `fk_venta_usuario` FOREIGN KEY (`idusuario`) REFERENCES `usuario` (`idusuario`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
